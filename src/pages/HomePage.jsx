46fef09332da9a3009da195deb2da96e6bdcdb28
silvio-apa <silvio.apanh@gmail.com>
2026-06-01 14:30:47 +0200

    Add home page image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,22 @@
 function HomePage() {
-    return (
-        <section>
-            <h1>Rick and Morty Character Database</h1>
+  return (
+    <section className="home-page">
+      <div className="home-text">
+        <h1>Rick and Morty Character Database</h1>
 
+        <p>
+          Welcome to my Rick and Morty database. Here you can explore different
+          characters and learn more about them.
+        </p>
+      </div>
 
-            <p> Welcome to my Rick and Morty database. Here you can explore
-                different characters and learn more about them.
-            </p>
-
-        </section>
-    );
-}   
+      <img
+        className="home-image"
+        src="/images/rick-and-morty-home.png"
+        alt="Rick and Morty"
+      />
+    </section>
+  );
+}
 
 export default HomePage;
